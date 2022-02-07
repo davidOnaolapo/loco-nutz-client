d8ad46a8bf56f60e4e0dd2fb1e608bb8bbf237b9
David Onaolapo <77420219+davidOnaolapo@users.noreply.github.com>
2022-02-07 02:41:44 -0500

    fix error phone

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,5 +1,5 @@
-// export const API_URL = "https://loco-nutz-server.herokuapp.com/address" || 'http://localhost:3001'
-export const API_URL = 'http://localhost:3001/address'
+export const API_URL = "https://loco-nutz-server.herokuapp.com/address" || 'http://localhost:3001'
+// export const API_URL = 'http://localhost:3001/address'
 
 
 
